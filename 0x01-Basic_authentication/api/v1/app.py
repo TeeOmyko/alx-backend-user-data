d177972658ebaf1813fc6bff696c8567dcff762f
Omiko <100170165+TeeOmyko@users.noreply.github.com>
2023-11-14 21:12:02 +0100

    changes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
+""" Module of Index views
 """
-Route module for the API
-"""
-from os import getenv
+from flask import jsonify, abort
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"status": "OK"})
 
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+# New endpoint for testing Forbidden error
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint() -> str:
+    """ GET /api/v1/forbidden
+    Raise a 403 Forbidden error
+    """
+    abort(403)
+
+# Error handler for 403 Forbidden
+@ app_views.errorhandler(403)
+def forbidden_error(error):
+    """ Handles 403 Forbidden error """
+    response = jsonify({"error": "Forbidden"})
+    response.status_code = 403
+    return response
